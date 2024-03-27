654c3c1787081dd134501f810ab1f5a85b5822e8
XavierRLX <renanlima2000.aer@gmail.com>
2024-03-27 16:14:48 -0300

    feat
    add loginPage and homePage

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage/index';
+
+
+function App() {
+  return (
+      <Lo/>
+      );
+}
+
+export default App;
